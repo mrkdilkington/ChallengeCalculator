db546d042ba2fce4a1458925ae6242b5a5731848
Jeffrey Gross <jeff@jeffgross.us>
2019-09-15 17:47:32 -0700

    First step in the calculator challenge. Supports a max of numbers (the rest are discarded). The calculator parses a string, splitting by comma, and summing up the numbers. Invalid/missing numbers are be converted to 0.

--- ChallengeCalculator/Program.cs
+++ ChallengeCalculator/Program.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ChallengeCalculator {
-    class Program {
-        static void Main(string[] args) {
-        }
+  class ChallengeCalculatorProgram {
+    [TestMethod]
+    public void StringCalculatorTest() {
+      const string testInput = "5,3";
+      int calcValue = StringCalculator(testInput);
+      Assert.IsTrue(calcValue == 8, string.Format("{0} input should equal 8.", testInput));
     }
+    public int StringCalculator(string input) {
+      // Split the string by comma, traverse the IEnumerable and add the parsed numbers to a list
+      IEnumerable<string> sNumbers = input.Split(',').Take(2);
+      List<int> numbers = new List<int>();
+      foreach (string sNumber in sNumbers) {
+        int number = 0;
+        int.TryParse(sNumber, out number);
+        numbers.Add(number);
+      }
+      // Sum the numbers in the list, and print out the addition of the numbers.
+      int sum = numbers.Sum();
+      string printOut = string.Format("{0} = {1}", string.Join("+", numbers), sum);
+      Console.WriteLine(printOut);
+      return sum;
+    }
+    static void Main(string[] args) {
+      ChallengeCalculatorProgram ccp = new ChallengeCalculatorProgram();
+      while (true) {
+        Console.WriteLine("Please enter a string to calculate.");
+        string input = Console.ReadLine();
+        ccp.StringCalculator(input);
+      }
+    }
+  }
 }

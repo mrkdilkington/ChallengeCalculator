291014ca6456b4ad6bbea93da74bb40049b040ed
Jeffrey Gross <jeff@jeffgross.us>
2019-09-16 17:10:12 -0700

    Added step 8 and comments. "Support multiple delimiters of any length use the format: //[{delimiter1}][{delimiter2}]...\n{numbers} e.g. //[*][!!][r9r]\n11r9r22*33!!44 will return 110 all previous formats should also be supported"

--- ChallengeCalculator/Program.cs
+++ ChallengeCalculator/Program.cs
@@ -35,16 +35,33 @@ namespace ChallengeCalculator {
       int calcValue = StringCalculator(testInput);
       Assert.IsTrue(calcValue == 66, "Value should equal 66.");
     }
+
+    public void StringCalculatorTestStep8() {
+      const string testInput = "//[*][!!][r9r]\n11r9r22*33!!44";
+      int calcValue = StringCalculator(testInput);
+      Assert.IsTrue(calcValue == 110, "Value should equal 110.");
+    }
     public int StringCalculator(string input) {
       // Split the string by comma, traverse the IEnumerable and add the parsed numbers to a list
       input = input ?? "";
       if (input.Substring(0, 2) == "//") {
+        // Split on double backslash and new line, with added slash for console strings
         List<string> cInput = input.Split(new string[2] { "//", "\\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
         if (cInput.Count == 2 && cInput[0].Length == 1) {
+          // Join the second element with the first element as the delimiter
           input = string.Join(",", cInput[1].Split(new string[1] { cInput[0] }, StringSplitOptions.RemoveEmptyEntries));
         }
+        // If the split count is two and the first element contains one of each bracket.
         else if (cInput.Count == 2 && cInput[0].Count(f => f == '[') == 1 && cInput[0].Count(f => f == ']') == 1) {
-          input = string.Join(",", cInput[1].Split(new string[1] { cInput[0].Trim('[',']') }, StringSplitOptions.RemoveEmptyEntries));
+          // Use the first element, minus the brackets, as a delimiter for the second element.
+          input = string.Join(",", cInput[1].Split(new string[1] { cInput[0].Trim('[', ']') }, StringSplitOptions.RemoveEmptyEntries));
+        }
+        // If the split count is two and the first element contains more than one of each bracket alone, and ][ but not []
+        else if (cInput.Count == 2 && cInput[0].Count(f => f == '[') > 1 && cInput[0].Count(f => f == ']') > 1 &&
+            cInput[0].Contains("][") && !cInput[0].Contains("[]")) {
+          // Split out the delimiters and use them in the subsequent step.
+          string[] delims = cInput[0].Trim('[', ']').Split(new string[1] { "][" }, StringSplitOptions.RemoveEmptyEntries);
+          input = string.Join(",", cInput[1].Split(delims, StringSplitOptions.RemoveEmptyEntries));
         }
       }
       IEnumerable<string> sNumbers = input.Split(new string[2] { ",", "\\n" }, StringSplitOptions.None);
